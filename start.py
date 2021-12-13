c907737cc70040332d2874509e9962974cc0c307
Mineplay <42841728+Mineplay3@users.noreply.github.com>
2021-12-13 14:07:53 -0500

    a

--- start.py
+++ start.py
@@ -1,11 +1,25 @@
+#### Imports
 import nextcord
 from nextcord.ext import commands
+import json
 
-token = None
-prefix = None
+#### Settings
+while open("settings.json", "r") as settings:
+    settings = settings.load()
 
+    print(settings)
+    token = None
+    prefix = None
+
+
+#### Initial Setup
 nextcord.ext.commands.Bot(command_prefix=prefix, help_command=None)
 
+#### Errors
+
+#### On Ready
+
+#### Starup
 if __name__ == '__main__':
     client.run(token)
 
